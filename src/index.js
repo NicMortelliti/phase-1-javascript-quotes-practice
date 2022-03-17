a230291c9e488ec8a5e9989ab9677dd235fce5a5
NicMortelliti <npmortelliti@gmail.com>
2022-03-17 16:22:11 -0700

    Add a quote constructor function

---
+++ src/index.js
@@ -0,0 +1,36 @@
+function quoteCardConstructor(quote, footer, likes) {
+  const li = document.createElement("li");
+  const blockQuote = document.createElement("blockquote");
+  const p = document.createElement("p");
+  const foot = document.createElement("footer");
+  const br = document.createElement("br");
+  const buttonSuccess = document.createElement("button");
+  const buttonSuccessSpan = document.createElement("span");
+  const buttonDelete = document.createElement("button");
+
+  li.setAttribute("class", "quote-card");
+  blockQuote.setAttribute("class", "blockquote");
+  p.setAttribute("class", "mb-0");
+  foot.setAttribute("class", "blockquote-footer");
+  buttonSuccess.setAttribute("class", "btn-success");
+  buttonDelete.setAttribute("class", "btn-danger");
+
+  p.textContent = quote;
+  foot.textContent = footer;
+  buttonSuccess.textContent = "Likes: ";
+  buttonSuccessSpan.textContent = likes;
+  buttonDelete.textContent = "Delete";
+
+  li.append(blockQuote);
+  blockQuote.append(p);
+  blockQuote.append(foot);
+  blockQuote.append(br);
+  blockQuote.append(buttonSuccess);
+  blockQuote.append(buttonDelete);
+  buttonSuccess.append(buttonSuccessSpan);
+
+  const body = document.getElementsByTagName("body")[0];
+  body.appendChild(li);
+}
+
+quoteCardConstructor("I'm saying something", "This is a footer", 3);
